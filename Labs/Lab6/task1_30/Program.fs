6c1bc8160582b8d788bca2e32bc0d8d80a417574
Zed Null <rustamxaxyk@gmail.com>
2022-03-20 21:23:09 +0300

    Лаба 6 задача 1_30

---
+++ Labs/Lab6/task1_30/Program.fs
@@ -0,0 +1,21 @@
+﻿open Functions
+
+let rec localMax list index =
+    match list with
+    | a::b::c::t when index = 1 -> b > a && b > c
+    | a::b::[] when index = 1 -> b > a
+    | a::b::t when index = 0 -> a > b
+    | _ -> 
+        let newIndex = index - 1
+        let newList = list.Tail
+        localMax newList newIndex
+
+[<EntryPoint>]
+let main argv =
+    let n = argv[0] |> int
+    let index = argv[1] |> int
+
+    let list = readList n
+
+    printfn "%b"(localMax list index)
+    0
