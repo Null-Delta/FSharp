78148b2b12b1c3e6d1e0b6ce35c1ccc8fe5987ea
Zed Null <rustamxaxyk@gmail.com>
2022-03-20 20:23:03 +0300

    Лаба 6 задача 11

--- Labs/Helper/Library.fs
+++ Labs/Helper/Library.fs
@@ -1,5 +1,10 @@
 ﻿namespace Helper
 
 module Functions =
-    let hello name =
-        printfn "Hello %s" name
+    let rec readList size =
+    if size = 0 then []
+    else 
+        let head = System.Convert.ToInt32(System.Console.ReadLine())
+        let tail = readList (size - 1)
+        head::tail
+
---
+++ Labs/Lab6/task11/Program.fs
@@ -0,0 +1,19 @@
+﻿open Helper.Functions
+
+let rec convert list (func: int -> int -> int -> int) =
+    match list with 
+    | a::b::c::t -> (func a b c) :: (convert t func)
+    | a::b::[] -> (func a b 1) :: (convert [] func)
+    | a::[] -> (func a 1 1) :: (convert [] func)
+    | [] -> []
+
+let cond x y z =
+    x + y + z
+
+[<EntryPoint>]
+let main argv =
+    let n = argv[0] |> int
+    let list = readList n
+    
+    printfn "%A"(convert list cond)
+    0
