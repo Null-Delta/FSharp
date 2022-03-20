71b2e3ebe1c2c6b5a821f334bb5f9da0ed95f8c0
Zed Null <rustamxaxyk@gmail.com>
2022-03-20 22:53:38 +0300

    Лаба 5 задача 14

---
+++ Labs/lab5/task14/Program.fs
@@ -0,0 +1,17 @@
+﻿open Functions
+
+let processDividers x (action: int -> int -> int) = 
+    let rec _getDividers init result =
+        match init with
+        | value when value = x -> action value result
+        | _ -> 
+            let nextInit = init + 1
+            let nextResult = if x % init = 0 then action result init else result
+            _getDividers nextInit nextResult
+    _getDividers 1 0
+
+[<EntryPoint>]
+let main argv =
+    let input = argv[0] |> int
+    printfn "%d"(processDividers input (fun x y -> x + y))
+    0
