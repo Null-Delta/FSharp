ab9eff7c1aa778662af18719d5d05df175238455
Zed Null <rustamxaxyk@gmail.com>
2022-03-20 21:20:09 +0300

    Лаба 6 задача 1_27

---
+++ Labs/Lab6/task1_27/Program.fs
@@ -0,0 +1,10 @@
+﻿open Functions
+
+let rec makeMove (list: int list) = list.Tail@[list.Head]
+
+[<EntryPoint>]
+let main argv =
+    let n = argv[0] |> int
+    let list = readList n
+    printfn "%A"(makeMove list)
+    0
