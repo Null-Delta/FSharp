0b3cf7a878465c0cc259af67b3051ae37c0e90c2
Zed Null <rustamxaxyk@gmail.com>
2022-03-20 21:47:14 +0300

    Лаба 5 задача 11

---
+++ Labs/lab5/task11/Program.fs
@@ -0,0 +1,14 @@
+﻿open Functions
+
+let answer lang =
+    match lang with
+    | "F#" -> "Подлиза"
+    | "Php" | "Python" -> "Кринж"
+    | "Swift" -> "Сразу видно - хороший человек"
+    | other -> $"Впервые слышу о {other}"
+
+[<EntryPoint>]
+let main argv =
+    let lang = argv[0]
+    printfn "%s"(answer lang)
+    0
