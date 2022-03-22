0e99ce37aa515bae84d4db93bef4b6557421570c
Zed Null <rustamxaxyk@gmail.com>
2022-03-22 20:20:26 +0300

    Лаба 5 задача 15

---
+++ Labs/lab5/task15/Program.fs
@@ -0,0 +1,12 @@
+﻿open Functions
+
+let processUndividers x (action: int -> int -> int) init = 
+    convolution (fun v -> 
+        convolution (fun t -> v % t = 0 && x % t = 0) (fun v1 v2 -> v2) 1 v = 1 
+    ) action 1 x
+
+[<EntryPoint>]
+let main argv =
+    let input = argv[0] |> int
+    printfn "%d"(processUndividers input (fun x y -> x + y) 0)
+    0
