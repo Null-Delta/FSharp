f52157ec5873d5319fb05d98fa955c3a490bc5a1
Zed Null <rustamxaxyk@gmail.com>
2022-03-25 14:16:20 +0300

    Лаба 7 задача 13

---
+++ Labs/lab7/task13/Program.fs
@@ -0,0 +1,31 @@
+﻿open System
+open Functions
+
+let isMax (list: int list) index = 
+    let min = List.fold (fun state value -> min value state) list.Head list.Tail
+    
+    let front = 
+        List.fold (fun (state: bool * int list) (value: int) ->
+            match state with
+            | (s, v) when s && value <> min -> (true, value::v)
+            | (_, v) when value = min -> (false, v) 
+            | (s,v) -> (false, v)
+        ) (true, []) list |> snd
+
+    let back = 
+        List.fold (fun (state: bool * int list) (value: int) ->
+            match state with
+            | (s, v) when s -> (true, v@[value])
+            | (s, v) when not s && value = min -> (true, v@[value]) 
+            | (s,v) -> (false, v)
+        ) (false, []) list |> snd
+
+    back@front
+    
+[<EntryPoint>]
+let main argv =
+    let n = argv[0] |> int
+
+    let list = readList n
+    printfn "%A"(isMax list 5)
+    0
