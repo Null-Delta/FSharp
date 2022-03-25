235f5ea688cc6fec98927e27c20fad85973c78a6
Zed Null <rustamxaxyk@gmail.com>
2022-03-25 10:10:56 +0300

    Лаба 7 задача 23

---
+++ Labs/lab7/task23/Program.fs
@@ -0,0 +1,18 @@
+﻿open Functions
+open System
+
+let find2Min (list: int list) = 
+    List.fold (fun (state: int * int) (value: int) -> 
+        match value with
+        | _ when fst state > value || snd state > value -> (value, min (fst state) (snd state))
+        | _ -> state
+    ) (list.Head, list.Tail.Head) list.Tail.Tail
+
+[<EntryPoint>]
+let main argv =
+    let n = argv[0] |> int
+
+    let list = readList n
+    
+    printfn "%A"(find2Min list)
+    0
