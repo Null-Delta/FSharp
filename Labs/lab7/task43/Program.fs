c1e301bb944122d9988eb270a68dbb521e5e5d2d
Zed Null <rustamxaxyk@gmail.com>
2022-03-25 10:24:29 +0300

    Лаба 7 задача 43

---
+++ Labs/lab7/task43/Program.fs
@@ -0,0 +1,19 @@
+﻿open Functions
+open System
+
+let minCount (list: int list) = 
+    snd (List.fold (fun (state: int * int) (value: int) -> 
+        match value with
+        | _ when fst state = value -> (fst state, snd state + 1)
+        | _ when fst state > value -> (value, 1)
+        | _ -> state
+    ) (list.Head, 1) list.Tail)
+
+[<EntryPoint>]
+let main argv =
+    let n = argv[0] |> int
+
+    let list = readList n
+    
+    printfn "%A"(minCount list)
+    0
