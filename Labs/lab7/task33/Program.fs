318ff4f06cea492d6045d2aaf50e9f28e1737d34
Zed Null <rustamxaxyk@gmail.com>
2022-03-25 10:15:59 +0300

    Лаба 7 задача 33

---
+++ Labs/lab7/task33/Program.fs
@@ -0,0 +1,18 @@
+﻿open Functions
+open System
+
+let isToToTo (list: int list) = 
+    fst (List.fold (fun (state: bool * int) (value: int) -> 
+        match value with
+        | _ when fst state = false -> state
+        | _ -> ( snd state % 2 <> value % 2, value)
+    ) (true, list.Head) list.Tail)
+
+[<EntryPoint>]
+let main argv =
+    let n = argv[0] |> int
+
+    let list = readList n
+    
+    printfn "%A"(isToToTo list)
+    0
