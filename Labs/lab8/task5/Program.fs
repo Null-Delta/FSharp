c6f9fce70da6e62ebd7f0705b8204c8ff3d1cea1
Zed Null <rustamxaxyk@gmail.com>
2022-04-14 23:57:28 +0300

    Лаба 8 задача 5 и 6

---
+++ Labs/lab8/task5/Program.fs
@@ -0,0 +1,15 @@
+﻿type DriverLicense(sn: string,n: string,bd: string,ed: string,c: string list,i: int) =
+    let mutable surname = sn
+    let mutable name = n
+    let mutable birthDay = bd
+    let mutable endDate = ed
+    let mutable categories = c
+    let mutable id = i
+    override this.ToString() = 
+        $"Права №{id}\nФИО: {surname} {name}\nДата рождения: {birthDay}\nКатегории: {categories}\nДата окончания децствия прав: {endDate}"
+
+[<EntryPoint>]
+let main argv =
+    let license = DriverLicense("Нагалевский", "Артем", "01.01.1001", "02.01.1001", ["A";"B"], 104564)
+    printfn "%s"(license.ToString())
+    0
