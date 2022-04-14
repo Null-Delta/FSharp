1824981563c2c1eb8673070d0d0359aa6d0b7e85
Zed Null <rustamxaxyk@gmail.com>
2022-04-15 00:10:33 +0300

    Лаба 8 задача 8

--- Labs/lab8/task5/Program.fs
+++ Labs/lab8/task5/Program.fs
@@ -1,12 +1,46 @@
-﻿type DriverLicense(sn: string,n: string,bd: string,ed: string,c: string list,i: int) =
-    member this.surname = sn
-    member this.name = n
-    member this.birthDay = bd
-    member this.endDate = ed
-    member this.categories = c
-    member this.id = i
+﻿open System.Text.RegularExpressions
+open System
+
+type DriverLicense(sn: string,n: string,bd: string,ed: string,c: string,i: int) =
+    member this.surname
+        with get() = sn
+        and set(newValue: string) =
+            if (Regex.IsMatch (newValue, @"[а-яА-Я]"))
+            then this.surname <- newValue
+            else printfn "%s"("Ошибка формата ввода")
+    member this.name
+        with get() = n
+        and set(newValue: string) =
+            if (Regex.IsMatch (newValue, @"[а-яА-Я]"))
+            then this.name <- newValue
+            else printfn "%s"("Ошибка формата ввода")
+    member this.birthDay
+        with get() = bd
+        and set(newValue: string) =
+            if (Regex.IsMatch (newValue, @"[0-9]{2}.[0-9]{2}.[0-9]{4}"))
+            then this.birthDay <- newValue
+            else printfn "%s"("Ошибка формата ввода")
+    member this.endDate
+        with get() = ed
+        and set(newValue: string) =
+            if (Regex.IsMatch (newValue, @"[0-9]{2}.[0-9]{2}.[0-9]{4}"))
+            then this.endDate <- newValue
+            else printfn "%s"("Ошибка формата ввода")
+    member this.category
+        with get() = c
+        and set(newValue: string) =
+            if (Regex.IsMatch (newValue, @"(A|B|C|D|E)"))
+            then this.category <- newValue
+            else printfn "%s"("Ошибка формата ввода")
+    member this.id 
+        with get() = i
+        and set(newValue: int) =
+            if (Regex.IsMatch (newValue |> string, @"[0-9]{6}"))
+            then this.id <- newValue
+            else printfn "%s"("Ошибка формата ввода")
+
     override this.ToString() = 
-        $"Права №{id}\nФИО: {this.surname} {this.name}\nДата рождения: {this.birthDay}\nКатегории: {this.categories}\nДата окончания децствия прав: {this.endDate}"
+        $"Права №{id}\nФИО: {this.surname} {this.name}\nДата рождения: {this.birthDay}\nКатегория: {this.category}\nДата окончания децствия прав: {this.endDate}"
     override this.Equals(obj1) = 
         match obj1 with
         | :? DriverLicense as license ->
@@ -15,7 +49,9 @@
 
 [<EntryPoint>]
 let main argv =
-    let license = DriverLicense("Нагалевский", "Артем", "01.01.1001", "02.01.1001", ["A";"B"], 104564)
-    let license2 = DriverLicense("Нагалевский", "Артем", "01.01.1001", "02.01.1001", ["A";"B"], 104564)
+    let license = DriverLicense("Нагалевский", "Артем", "01.01.1001", "02.01.1001", "A", 104564)
+    let license2 = DriverLicense("Нагалевский", "Артем", "01.01.1001", "02.01.1001", "B", 104564)
+
+    license.surname <- "102945"
     printfn "%A"(license = license2)
     0
