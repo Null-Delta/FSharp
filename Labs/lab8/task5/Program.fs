897968ad204ed67acf623ac9734b15a2627bfee8
Zed Null <rustamxaxyk@gmail.com>
2022-04-15 00:02:22 +0300

    Лаба 8 задача 7

--- Labs/lab8/task5/Program.fs
+++ Labs/lab8/task5/Program.fs
@@ -1,15 +1,21 @@
 ﻿type DriverLicense(sn: string,n: string,bd: string,ed: string,c: string list,i: int) =
-    let mutable surname = sn
-    let mutable name = n
-    let mutable birthDay = bd
-    let mutable endDate = ed
-    let mutable categories = c
-    let mutable id = i
+    member this.surname = sn
+    member this.name = n
+    member this.birthDay = bd
+    member this.endDate = ed
+    member this.categories = c
+    member this.id = i
     override this.ToString() = 
-        $"Права №{id}\nФИО: {surname} {name}\nДата рождения: {birthDay}\nКатегории: {categories}\nДата окончания децствия прав: {endDate}"
+        $"Права №{id}\nФИО: {this.surname} {this.name}\nДата рождения: {this.birthDay}\nКатегории: {this.categories}\nДата окончания децствия прав: {this.endDate}"
+    override this.Equals(obj1) = 
+        match obj1 with
+        | :? DriverLicense as license ->
+            license.id = this.id
+        | _ -> false
 
 [<EntryPoint>]
 let main argv =
     let license = DriverLicense("Нагалевский", "Артем", "01.01.1001", "02.01.1001", ["A";"B"], 104564)
-    printfn "%s"(license.ToString())
+    let license2 = DriverLicense("Нагалевский", "Артем", "01.01.1001", "02.01.1001", ["A";"B"], 104564)
+    printfn "%A"(license = license2)
     0
