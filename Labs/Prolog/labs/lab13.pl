8d275a1ab3205504c5ec0060d3a2d8d825d62fdf
Zed Null <rustamxaxyk@gmail.com>
2022-05-01 00:16:14 +0300

    Пролог лаба 13 задача 45

--- Labs/Prolog/labs/lab13.pl
+++ Labs/Prolog/labs/lab13.pl
@@ -1,10 +1,11 @@
-printArrayWithStep2([], Index).
+%task39
+printArrayWithStep2([], _).
 printArrayWithStep2([H|T], Index) :-
     0 is Index mod 2,
     NewIndex is Index + 1,
     write(H),write(', '),
     printArrayWithStep2(T, NewIndex),!.
-printArrayWithStep2([H|T], Index) :-
+printArrayWithStep2([_|T], Index) :-
     NewIndex is Index + 1,
     printArrayWithStep2(T, NewIndex),!.
 
@@ -13,3 +14,12 @@ printArrayParts(List) :-
     nl,
     printArrayWithStep2(List,0).
     
+%task45
+findSum([],_,_,R,R).
+findSum([H|T],A,B,R,LR) :-
+    H =< B,
+    H >= A,
+    NewLR is LR + H,
+    findSum(T,A,B,R,NewLR).
+findSum([H|T],A,B,R,LR) :- findSum(T,A,B,R,LR).
+findSum(List,A,B,R) :- findSum(List,A,B,R,0).
