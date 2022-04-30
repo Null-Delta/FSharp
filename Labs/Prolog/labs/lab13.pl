151e81c86516b7c1941a3748ad9929f05c01d1ca
Zed Null <rustamxaxyk@gmail.com>
2022-05-01 00:31:16 +0300

    Пролог лаба 13 задача 51

--- Labs/Prolog/labs/lab13.pl
+++ Labs/Prolog/labs/lab13.pl
@@ -21,5 +21,47 @@ findSum([H|T],A,B,R,LR) :-
     H >= A,
     NewLR is LR + H,
     findSum(T,A,B,R,NewLR).
-findSum([H|T],A,B,R,LR) :- findSum(T,A,B,R,LR).
+findSum([_|T],A,B,R,LR) :- findSum(T,A,B,R,LR).
 findSum(List,A,B,R) :- findSum(List,A,B,R,0).
+
+%task51
+contains([],_) :- fail,!.
+contains([H|T],V) :-
+    (V is H,!;contains(T,V)).
+
+reverseList([],Result, Local) :- Result = Local.
+reverseList([H|T],Result, Local) :-
+    reverseList(T, Result, [H|Local]).
+reverseList(List,Result) :- reverseList(List,Result, []).
+
+pushBack(List,V,NewList) :-
+    reverseList(List, ReversedList),
+    reverseList([V|ReversedList], NewList).
+
+generateList1([],R,R).
+generateList1([H|T], Result, LocalResult) :-
+    not(contains(LocalResult,H)),
+    pushBack(LocalResult, H, NewLocal),
+    generateList1(T,Result,NewLocal),!.
+generateList1([_|T], Result, LocalResult) :-
+    generateList1(T,Result,LocalResult),!.
+generateList1(List,Result) :- generateList1(List, Result, []).
+
+findCount([],_,R,R).
+findCount([H|T],V,R,LR) :-
+    H is V, 
+    NewLR is LR + 1,
+    findCount(T,V,R,NewLR),!;
+    findCount(T,V,R,LR).
+findCount(List,V,R) :- findCount(List,V,R,0).
+
+generateList2(_,[],R,R).
+generateList2(List,[H|T],List2,LocalList) :-
+    findCount(List,H,Count),
+    pushBack(LocalList, Count, NewLocal),
+    generateList2(List,T,List2,NewLocal),!.
+generateList2(List, List1, List2) :- generateList2(List,List1,List2,[]).
+
+calculateLists(List,L1,L2) :-
+    generateList1(List,L1),
+    generateList2(List,L1,L2).
