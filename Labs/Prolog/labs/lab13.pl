cb97f6563f17b1c42b56a68593f8c3e0df0dd3cf
Zed Null <rustamxaxyk@gmail.com>
2022-05-01 00:11:26 +0300

    Пролог лаба 13 задача 39

---
+++ Labs/Prolog/labs/lab13.pl
@@ -0,0 +1,15 @@
+printArrayWithStep2([], Index).
+printArrayWithStep2([H|T], Index) :-
+    0 is Index mod 2,
+    NewIndex is Index + 1,
+    write(H),write(', '),
+    printArrayWithStep2(T, NewIndex),!.
+printArrayWithStep2([H|T], Index) :-
+    NewIndex is Index + 1,
+    printArrayWithStep2(T, NewIndex),!.
+
+printArrayParts(List) :-
+    printArrayWithStep2(List,1),
+    nl,
+    printArrayWithStep2(List,0).
+    
